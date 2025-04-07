03758d68b2733fd1b9001aceebb74c915eb651c9
Nguyễn Hồ Bảo Khang <120096785+Thesider@users.noreply.github.com>
2025-04-08 05:49:31 +0700

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,17 @@
+function upDate(previewPic){
+
+    const imageDiv = document.getElementById("image");
+
+    imageDiv.style.backgroundImage = "url('" + previewPic.src + "')";
+
+    imageDiv.innerHTML = previewPic.alt;
+}
+
+function unDo(){
+
+    const imageDiv = document.getElementById("image");
+
+    imageDiv.style.backgroundImage = "url('')";
+
+    imageDiv.innerHTML = "Hover over an image below to display here.";
+}
